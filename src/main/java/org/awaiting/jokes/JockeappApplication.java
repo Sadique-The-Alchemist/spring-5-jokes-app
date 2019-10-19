c52fe79e50141215b271e174606e33193f42350f
gitsadique <sadique.pal@gmail.com>
2019-10-19 06:41:31 +0530

    creating spring boot project closes #1

---
+++ src/main/java/org/awaiting/jokes/JockeappApplication.java
@@ -0,0 +1,13 @@
+package org.awaiting.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JockeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JockeappApplication.class, args);
+    }
+
+}
